31995d3cfc98263891d080e5454a9335982c2aff
t2ols <t2ols@hanmail.net>
2021-01-21 17:53:23 +0900

    update

--- keggle_learning/1.py
+++ keggle_learning/1.py
@@ -1,4 +1,5 @@
 #invite people for the Kaggle party
+import torch
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -8,4 +9,12 @@ from sklearn.preprocessing import StandardScaler
 from scipy import stats
 import warnings
 warnings.filterwarnings('ignore')
-%matplotlib inline
+# %matplotlib inline
+
+is_cuda = False
+
+if torch.cuda.is_available():
+    print('CUDA available')
+    is_cuda = True
+
+print( is_cuda )    
